a95a840e94fa73a758447957c290f34556bc003c
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2016-12-14 14:12:29 +0100

    demo cameraviewjs

---
+++ demoCameraViewJS/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo cp cameraviewjs /usr/local/bin/
+sudo cp cameraviewjs.conf /etc/init.d/cameraviewjs
+sudo cp cameraserver.conf /etc/init.d/cameraserver
+sudo cp cameraserver.daemon /usr/local/bin/
