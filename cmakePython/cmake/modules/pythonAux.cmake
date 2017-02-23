942d46421f17f4d1546a9099b7a6fb9c3eeb016f
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2017-02-23 11:00:19 +0100

    prepared cmake to python

--- cmakePython/cmake/modules/pythonAux.cmake
+++ cmakePython/cmake/modules/pythonAux.cmake
@@ -29,7 +29,7 @@ endmacro()
 
 
 
-#macro to switch pythonInterp
+#macro configure files for python2 and python3
 macro(configure_file_python input output)
 
   usePython(2)
@@ -41,7 +41,7 @@ macro(configure_file_python input output)
 endmacro()
 
 
-#macro to switch pythonInterp
+#macro configure al filoes of a module for python2 and python3
 macro(configure_module_python module)
   file(GLOB_RECURSE files RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/${ModuleName}/ *.in)
 
@@ -55,7 +55,7 @@ macro(configure_module_python module)
 endmacro()
 
 
-# MACRO to get the list of subdirectories
+# MACRO to copy to binary directory all files of python modulefor python2 and 3 
 macro(copy_to_binary_python target ModuleName)
 
   if (NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/py2/${ModuleName})
@@ -90,22 +90,50 @@ macro(copy_to_binary_python target ModuleName)
 endmacro()
 
 
+
+MACRO to install python modules for python2 and 3 
 macro (install_python module component)
-  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/py2/jderobotComm
+  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/py2/${module}
 
     DESTINATION ${PYTHON2_MODULE_PATH}
 
-    COMPONENT core
+    COMPONENT ${component}
 
   )
 
-  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/py3/jderobotComm
+  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/py3/${module}
 
     DESTINATION ${PYTHON3_MODULE_PATH}
 
-    COMPONENT core
+    COMPONENT ${component}
 
   )
-
-
 endmacro()
+
+
+# Find if a Python module is installed
+# Found at http://www.cmake.org/pipermail/cmake/2011-January/041666.html
+# To use do: find_python_module(PyQt5 REQUIRED)
+function(find_python_module module)
+  string(TOUPPER ${module} module_upper)
+  if(NOT PY_${module_upper})
+    if(ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED")
+      set(${module}_FIND_REQUIRED TRUE)
+    endif()
+    # A module's location is usually a directory, but for binary modules
+    # it's a .so file.
+    message(STATUS ${module})
+    set(python_cmd ${PYTHON_EXECUTABLE})
+    execute_process(COMMAND "${python_cmd}" "-c" 
+      "import re, ${module}; print(re.compile('/__init__.py.*').sub('',${module}.__file__))"
+      RESULT_VARIABLE _${module}_status  
+        OUTPUT_VARIABLE _${module}_location
+        ERROR_QUIET 
+      OUTPUT_STRIP_TRAILING_WHITESPACE)   
+    if(NOT _${module}_status )
+      set(PY_${module_upper} ${_${module}_location} CACHE STRING 
+        "Location of Python module ${module}")
+    endif(NOT _${module}_status )
+  endif(NOT PY_${module_upper})
+  find_package_handle_standard_args(PY_${module} DEFAULT_MSG PY_${module_upper})
+endfunction(find_python_module)
